0d86dc3eb83e6c8af452babc2e7abd5415b45047
Stan Brouwer <stanbrouwer2000@hotmail.com>
2026-02-07 19:55:09 +0100

    Add initial Python code and notes

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 
 # THis is our code
 
-print ("I love Git")
-print ("I am learning Git")
-print ("I am learning Github")
-print ("I am learning Git and Github")
+#Import and configure the required modules.
+# Define required imports
+import pandas as pd
+import numpy as np
+import sys
+import re
+# These set pandas max column and row display in the notebook
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.max_rows', 50)
 
 def main():
     print("Experimental Python Project")
