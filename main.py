ee4c9fb4583f6140313deedadae5c914ee22c08d
Stan Brouwer <stanbrouwer2000@hotmail.com>
2026-02-07 16:11:23 +0100

    Add executable Python entry point

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ print ("I love Git")
 print ("I am learning Git")
 print ("I am learning Github")
 print ("I am learning Git and Github")
+
+def main():
+    print("Experimental Python Project")
+    print("This repository tracks my progress learning Python and Git.")
+
+if __name__ == "__main__":
+    main()
